7b178bf62c688761816c4f6fb2b20105dc5a5d18
krishnaaahhhhhh <Krishnasinghrathour9628@gmail.com>
2026-07-09 19:41:59 +0530

    Resolve layout merge conflict

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,14 @@
 import './globals.css';
 import { Encode_Sans } from 'next/font/google';
-import localFont from "next/font/local";
 import AppShell from '@/components/layouts/AppShell';
 import LayoutBackground from '@/components/LayoutBackground';
 import ThemeRegistry from '@/components/ThemeRegistry';
 import MobileBottomNav from '@/components/navbar/MobileNavBar';
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
 
 const encodeSans = Encode_Sans({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
