69a8180453708bcb5b5310da485ed218df1489c4
Sarahfaatima <sarahfatima1924@gmail.com>
2026-07-29 00:22:54 +0530

    FKP-117 #comment implement radius and spacing utils

---
+++ src/utils/spacing.js
@@ -0,0 +1,23 @@
+export const spacing = {
+  none: "0px",
+
+  xxs: "2px",
+
+  xs: "4px",
+
+  sm: "8px",
+
+  md: "12px",
+
+  lg: "16px",
+
+  xl: "24px",
+
+  xxl: "32px",
+
+  xxxl: "40px",
+
+  huge: "64px",
+
+  massive: "80px",
+};
