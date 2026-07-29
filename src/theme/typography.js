4fbf87513e4055bb0a03ce883aec96a04245ae84
VaishnaviAsthaG <167325432+VaishnaviAsthaG@users.noreply.github.com>
2026-07-29 17:18:48 +0530

    FEAT: Add utils file of color.js and typography.js

---
+++ src/theme/typography.js
@@ -0,0 +1,56 @@
+export const typography = {
+  display1: {
+    fontSize: "60px",
+    fontWeight: 700,
+  },
+
+  display2: {
+    fontSize: "48px",
+    fontWeight: 700,
+  },
+
+  display3: {
+    fontSize: "24px",
+    fontWeight: 700,
+  },
+
+  heading1: {
+    fontSize: "20px",
+    fontWeight: 400,
+  },
+
+  heading2: {
+    fontSize: "18px",
+    fontWeight: 400,
+  },
+
+  label1: {
+    fontSize: "16px",
+    fontWeight: 500,
+  },
+
+  label2: {
+    fontSize: "14px",
+    fontWeight: 500,
+  },
+
+  label3: {
+    fontSize: "12px",
+    fontWeight: 400,
+  },
+
+  paragraph1: {
+    fontSize: "20px",
+    fontWeight: 400,
+  },
+
+  paragraph2: {
+    fontSize: "16px",
+    fontWeight: 400,
+  },
+
+  paragraph3: {
+    fontSize: "14px",
+    fontWeight: 400,
+  },
+};
